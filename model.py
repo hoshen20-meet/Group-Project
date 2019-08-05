7bffac40ece1d562a5d5bb000980f9c79deb97b5
myname21-meet <myname21@meet.mit.edu>
2019-08-05 05:10:11 -0400

    group database

---
+++ model.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer , String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+
+
+Base = declarative_base()
+
+
+class Customers(Base):
+   __tablename__ = 'customers'
+   customer_id = Column(Integer, primary_key=True)
+   purchased_books = Column(String)
+   name = Column(String)
+   nickname = Column(String)
+   password = Column(String)
+   
+
+class Books(Base):
+    __tablename__ = 'Books'
+    Book_id = Column(Integer, primary_key=True)
+    title = Column(String)
+    price = Column(String)
+   
