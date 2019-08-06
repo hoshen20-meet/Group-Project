76b276aa0c90420ec83648b4bd9da9581431cfce
myname21-meet <myname21@meet.mit.edu>
2019-08-06 06:02:22 -0400

    message

--- app.py
+++ app.py
@@ -32,9 +32,9 @@ def event_details():
 def elements():
     return render_template('elements.html')
 
-@app.route('/blog')
-def blog():
-    return render_template('blog.html')
+@app.route('/donate')
+def donate():
+    return render_template('donate.html')
 
 @app.route('/single-blog')
 def singlr_blog():
